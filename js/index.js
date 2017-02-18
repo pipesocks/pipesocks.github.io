0279899bd02b76509ecbe347b3d6e2626f0b37fb
bdbai <htbai1998m@hotmail.com>
2017-02-19 00:09:42 +0800

    Refactor
    Add favicon
    Polish HTML structure
    Use BootCDN and ustc Google font source
    Use flex layout
    Small fixes

--- js/index.js
+++ js/index.js
@@ -1,90 +1,95 @@
-var platformList = {
-  "linux": {
-    "version": "2.2",
-    "date"   : "2017/02/08",
-    "package": "zip"
-  },
-  "macOS": {
-    "version": "2.2",
-    "date"   : "2017/02/08",
-    "package": "dmg"
-  },
-  "win": {
-    "version": "2.2",
-    "date"   : "2017/02/08",
-    "package": "exe"
-  },
-  "github": {
-    "package": "",
-    "url": "https://github.com/pipesocks/pipesocks"
+'use strict';
+((window, $) => {
+  const platformList = {
+    "linux": {
+      "version": "2.2",
+      "date": "2017/02/08",
+      "package": "zip"
+    },
+    "macOS": {
+      "version": "2.2",
+      "date": "2017/02/08",
+      "package": "dmg"
+    },
+    "win": {
+      "version": "2.2",
+      "date": "2017/02/08",
+      "package": "exe"
+    },
+    "github": {
+      "package": "",
+      "url": "https://github.com/pipesocks/pipesocks"
+    }
   }
-}
-var baseUrl = "https://coding.net/u/yvbbrjdr/p/pipesocks-release/git/raw/master/pipesocks-";
+  const baseUrl = "https://coding.net/u/yvbbrjdr/p/pipesocks-release/git/raw/master/pipesocks-";
 
-var hookListener = () => {
-  $("a[locate]").each((n,c) => {
-      $(c).on("click", () => {
-          navTo($(c).attr("locate"));
+  function hookListener() {
+    $("#nav_menu a[data-locate]").click(function (e) {
+      e.preventDefault();
+      navTo($(this).data("locate"));
+    });
+    $("tr[data-gotod]").each((n, c) => {
+      const $c = $(c);
+      const p = $c.data("gotod");
+      const url = `${platformList[p]["url"] || baseUrl}${platformList[p]["version"]}-${p}.${platformList[p]["package"]}`;
+      $c.children('td').click(e => {
+        e.preventDefault();
+        window.location.href = url;
       });
-  });
-  $("tr[gotod]").each((n,c) => {
-      var p = $(c).attr("gotod");
-      $(c).on(("click"), () => {
-          window.open(platformList[p]["url"] || baseUrl + platformList[p]["version"] + "-" + p + "." + platformList[p]["package"]);
-      });
-      $(c).children()[2].innerHTML = platformList[p]["version"] || "Latest";
-      $(c).children()[3].innerHTML = platformList[p]["date"] || "";
-  });
-  $("#openmenu").on("click", () => {
-      if ($("#nav_menu").css("display") == "none") {
-        $("#nav_menu").css("opacity", "0");
-        $("#nav_menu").css("display", "block");
-        $("#nav").css("height","18em");
-        setTimeout(() => {
-            $("#nav_menu").css("opacity", "1");
-        }, 3e2);
+      $c.children()[2].innerText = platformList[p]["version"] || "Latest";
+      $c.children()[3].innerText = platformList[p]["date"] || "";
+    });
+    $("#openmenu").click(e => {
+      e.preventDefault();
+      const $nav = $('#nav');
+      if ($nav.hasClass("nav-expanded")) {
+        $('#nav_menu').fadeOut(300);
+        $nav.removeClass('nav-expanded').css("height", "");
       } else {
-        $("#nav_menu").css("opacity", "0");
-        setTimeout(() => {
-            $("#nav").css("height","");
-            setTimeout(() => {
-              $("#nav_menu").css("display", "");
-              $("#nav_menu").css("opacity", "1");
-            }, 3e2);
-        }, 3e2);
+        $('#nav_menu').delay(300).fadeIn(300);
+        $nav.addClass('nav-expanded');
       }
-  });
-}
+    });
+  }
 
-document.onscroll = (e) => {
-    navSet();
-};
+  let scrollTriggered = false;
+  window.document.onscroll = e => {
+    if (scrollTriggered) return;
+    scrollTriggered = true;
+    setTimeout(() => {
+      scrollTriggered = false;
+      navSet()
+    }, 300);
+  };
 
-var navTo = (n) => {
-    $("body").animate({scrollTop:$("#"+n).offset().top}, 1e3);
+  function navTo(n) {
+    $("body").animate({ scrollTop: $("#" + n).offset().top }, 1e3);
     navSet(n);
-}
+  }
 
-var navSet = () => {
-    aHeight = window.pageYOffset;
+  function navSet() {
+    const aHeight = window.pageYOffset;
     $("#nav").css("background-color", "rgba(0,0,0," + ((aHeight / 500) > 0.8 ? 0.8 : (aHeight / 500)) + ")");
-    var isset = false;
-    $("a[locate]").each((n,c) => {
-        if (isset) {
-          $($(c).children()[0]).removeClass("active");
+    let isset = false;
+    $("a[data-locate]").each((n, c) => {
+      const $c = $(c);
+      const $navTarget = $(`#${$c.data("locate")}`);
+      if (isset) {
+        $c.parent().removeClass("active");
+      } else {
+        const partOffsetY = $navTarget.offset().top + ($navTarget.height() / 3 * 2);
+        if (aHeight <= partOffsetY) {
+          $c.parent().addClass("active");
+          isset = true;
         } else {
-          partOffsetY = $("#"+$(c).attr("locate")).offset().top + ($("#"+$(c).attr("locate")).height() / 3 * 2);
-          if (aHeight <= partOffsetY) {
-            $($(c).children()[0]).addClass("active");
-            isset = true;
-          } else {
-            $($(c).children()[0]).removeClass("active");
-          }
+          $c.parent().removeClass("active");
         }
+      }
     });
 
-}
+  }
 
-window.onload = () => {
+  $(() => {
     hookListener();
-}
+  });
+})(window, jQuery);
