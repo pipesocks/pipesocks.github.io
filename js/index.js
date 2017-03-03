8e48e5a37e23947e8309ccbd23ba915f8148c754
bdbai <htbai1998m@hotmail.com>
2017-03-03 21:18:25 +0800

    Extract release JSON

--- js/index.js
+++ js/index.js
@@ -1,65 +1,23 @@
 'use strict';
 ((window, $) => {
-  const platformList = {
-    "linux": {
-      "version": "2.3",
-      "date": "2017/02/26",
-      "package": "tar.xz"
-    },
-    "macOS": {
-      "version": "2.3",
-      "date": "2017/02/26",
-      "package": "dmg"
-    },
-    "win": {
-      "version": "2.3",
-      "date": "2017/02/26",
-      "package": "exe"
-    },
-    "iOS": {
-      "version": "×",
-      "date": "×",
-      "url": "https://itunes.apple.com/us/app/pipesocks/id1201534301?l=zh&ls=1&mt=8"
-    },
-    "github-desktop": {
-      "url": "https://github.com/pipesocks/pipesocks"
-    },
-    "github-iOS": {
-      "url": "https://github.com/pipesocks/pipesocks-iOS"
-    }
-  }
-  const baseUrl = "https://coding.net/u/yvbbrjdr/p/pipesocks-release/git/raw/master/pipesocks-";
-
   function hookListener() {
+    // Navbar click
     $("#nav_menu a[data-locate]").click(function (e) {
       e.preventDefault();
       navTo($(this).data("locate"));
     });
-    $("tr[data-gotod]").each((n, c) => {
-      const $c = $(c);
-      const p = $c.data("gotod");
-      var url;
-      if (platformList[p]["url"] == null) {
-        url = `${baseUrl}${platformList[p]["version"]}-${p}.${platformList[p]["package"]}`;
-      } else {
-        url = platformList[p]["url"];
-      }
-      $c.children('td').click(e => {
-        e.preventDefault();
-        window.location.href = url;
-      });
-      $c.children()[2].innerText = platformList[p]["version"] || "Latest";
-      $c.children()[3].innerText = platformList[p]["date"] || "";
+
+    // Navbar auto switch
+    $(window).scroll(e => {
+      window.requestAnimationFrame(navSet);
     });
+
+    // Toggle menu
     $("#openmenu").click(e => {
       e.preventDefault();
       $('#nav').toggleClass('nav-expanded');
     });
   }
-
-  window.document.onscroll = e => {
-    window.requestAnimationFrame(navSet);
-  };
 
   function navTo(n) {
     $("body").animate({ scrollTop: $("#" + n).offset().top }, 1e3);
@@ -85,10 +43,29 @@
         }
       }
     });
+  }
 
+  function loadPlatforms() {
+    $.getJSON('dist.json').then(res => {
+      const { baseUrl, platform } = res;
+      $("tr[data-gotod]").each((n, c) => {
+        const $c = $(c);
+        const p = $c.data("gotod");
+        const url = platform[p]["url"]
+          || `${baseUrl}${platform[p]["version"]}-${p}.${platform[p]["package"]}`;
+
+        $c.children('td').click(e => {
+          e.preventDefault();
+          window.location.href = url;
+        });
+        $c.children().eq(2).text(platform[p]["version"] || "Latest");
+        $c.children().eq(3).text(platform[p]["date"] || "");
+      });
+    });
   }
 
   $(() => {
+    loadPlatforms();
     hookListener();
   });
 })(window, jQuery);
